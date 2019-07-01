59f28a64a98f545a88c484d2d2869986f39ad1e4
qqcdn <heiwu789@gmail.com>
2019-07-02 05:59:39 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,14 +8,3 @@ $(document).ready(function() {
     return false;
   });
 });
-
-
-function createxmlHttpRequest() {
-    var xmlHttp;
-    if (window.ActiveXObject) {
-        xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
-    } else if (window.XMLHttpRequest) {
-        xmlHttp=new XMLHttpRequest();
-    }
-    return xmlHttp;
-}
