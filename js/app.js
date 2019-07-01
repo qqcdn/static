ddf67a389ae6ef791f2f99f0985f33cad76c8c25
qqcdn <heiwu789@gmail.com>
2019-07-02 05:57:46 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -20,29 +20,6 @@ function createxmlHttpRequest() {
     return xmlHttp;
 }
 
-function addFav() {
-    xmlHttp = createxmlHttpRequest();
-    xmlHttp.open("GET",'/favorite.html');
-    xmlHttp.send(null);
-    var url = window.location;
-    var title = document.title;
-    var ua = navigator.userAgent.toLowerCase();
-    if (ua.indexOf("360se") > -1) {
-        alert("由于360浏览器功能限制，请按 Ctrl+D 手动收藏！");
-    }else if (ua.indexOf("msie 8") > -1) {
-        window.external.AddToFavoritesBar(url, title); //IE8
-    }else if (document.all) {
-        try{
-            window.external.addFavorite(url, title);
-        }catch(e){
-            alert('您的浏览器不支持,请按 Ctrl+D 手动收藏!');
-        }
-    }else if (window.sidebar) {
-        window.sidebar.addPanel(title, url, "");
-    }else {
-        alert('您的浏览器不支持,请按 Ctrl+D 手动收藏!');
-    }
-}
 
 var kkDapCtrl = null;
 function kkGetDapCtrl() {
@@ -73,24 +50,3 @@ function kkGetDapCtrl() {
 	return kkDapCtrl;
 }
 
-function start(url) {
-  var dapCtrl=kkGetDapCtrl();
-  try {
-		var ver = dapCtrl.GetThunderVer("KANKAN", "INSTALL");
-		var type = dapCtrl.Get("IXMPPACKAGETYPE");
-		if(ver && type && ver >= 672 && type >= 2401)
-		{
-			dapCtrl.Put("sXmp4Arg", '"'+url+'"'+' /sstartfrom _web_xunbo /sopenfrom web_xunbo');
-		}	else {
-			//alert('请先更新迅雷看看！');
-				if(window.confirm("请先更新迅雷看看播放器\n\n点击“确定”下载并安装更新\n\n否则请点击“取消”")){
-	window.open("http://xmp.down.sandai.net/kankan/XMPSetup_5.1.18.3900-dl.exe");
-	}
-		}
-	} catch(e) {
-  	//alert('请先安装迅雷看看！');
-	if(window.confirm("请先安装迅雷看看播放器\n\n点击“确定”下载并安装软件\n\n否则请点击“取消”")){
-	window.open("http://xmp.down.sandai.net/kankan/XMPSetup_5.1.18.3900-dl.exe");
-	}
-	}
-}
