38834fd08d71cb6adf573da68b9c65d285f57dcf
qqcdn <heiwu789@gmail.com>
2019-07-03 05:35:54 +0800

    Create biaoqian.js

---
+++ js/biaoqian.js
@@ -0,0 +1,251 @@
+
+
+//首页
+function benyueremenbtzhongzi(){
+document.writeln("本月热门BT种子");
+}
+
+function benzhouremenbtzhongzi(){
+document.writeln("本周热门BT种子");
+}
+
+function jinriremenbtzhongzi(){
+document.writeln("今日热门BT种子");
+}
+
+function zuixinshoulucili(){
+document.writeln("最新收录磁力");
+}
+
+function yingpianpaihangbang(){
+document.writeln("2019影片排行榜");
+}
+
+function zuixinmingxing(){
+document.writeln("最新明星");
+}
+
+function zuixinfanhao(){
+document.writeln("最新番号");
+}
+
+function btzhongzileixing(){
+document.writeln("BT种子类型");
+}
+
+function shipin(){
+document.writeln("视频");
+}
+
+function yinpin(){
+document.writeln("音频");
+}
+
+function wendang(){
+document.writeln("文档");
+}
+
+function ruanjian(){
+document.writeln("软件/压缩包");
+}
+
+function tupian(){
+document.writeln("图片");
+}
+
+function gaoqing(){
+document.writeln("高清");
+}
+
+function youqinglianjie(){
+document.writeln("友情链接");
+}
+
+function gengduo(){
+document.writeln("更多 »");
+}
+
+
+//搜索页面
+function fenlei(){
+document.writeln("分类：");
+}
+
+function daxiao(){
+document.writeln("大小：");
+}
+
+function redu(){
+document.writeln("热度：");
+}
+
+function shoulu(){
+document.writeln("收录：");
+}
+
+function zuijin(){
+document.writeln("最近：");
+}
+
+function remensousuo(){
+document.writeln("热门搜索");
+}
+
+function tebietishi(){
+document.writeln("特别提示");
+}
+
+function tebietishi1(){
+document.writeln("本站不存储任何资源内容，只收集BT种子元数据和磁力链接，是一个完全合法的搜索引擎系统。 网站不提供种子下载服务，也不对BT种子真实性及合法性负责，请用户注意甄别！");
+}
+
+function paixu(){
+document.writeln("排序：");
+}
+
+function quanbu(){
+document.writeln("全部");
+}
+
+function yingshi(){
+document.writeln("影视");
+}
+
+function yinyue(){
+document.writeln("音乐");
+}
+
+function tuxiang(){
+document.writeln("图像");
+}
+
+function anzhuangbao(){
+document.writeln("安装包");
+}
+
+function yasuobao(){
+document.writeln("压缩包");
+}
+
+function qita(){
+document.writeln("其他");
+}
+
+function xiangsi(){
+document.writeln("相似");
+}
+
+function redu1(){
+document.writeln("热度");
+}
+
+function daxiao1(){
+document.writeln("大小");
+}
+
+function ruku(){
+document.writeln("入库");
+}
+
+function zuihou(){
+document.writeln("最近下载");
+}
+
+
+
+
+//内容页
+
+function btzhongzimingcheng(){
+document.writeln("BT种子名称");
+}
+
+function btzhongzijibenxinxi(){
+document.writeln("BT种子基本信息");
+}
+
+function xiazaidizhi(){
+document.writeln("下载地址");
+}
+
+function wendangliebiao(){
+document.writeln("文档列表");
+}
+
+function cainixihuan(){
+document.writeln("猜你喜欢");
+}
+
+function zuixincililianjie(){
+document.writeln("最新磁力链接");
+}
+
+function zhongzibiaoqian(){
+document.writeln("种子标签");
+}
+
+function zhongzipingjia(){
+document.writeln("种子评价");
+}
+
+function zuijinsousuo(){
+document.writeln("最近搜索");
+}
+
+function renqimingxing(){
+document.writeln("人气明星");
+}
+
+function zuixinfanhao(){
+document.writeln("最新番号");
+}
+
+function zhongzihaxi(){
+document.writeln("种子哈希：");
+}
+
+function wendangdaxiao(){
+document.writeln("文档大小：");
+}
+
+function zhongzifenlei(){
+document.writeln("种子分类：");
+}
+
+function wendangshuliang(){
+document.writeln("文档数量：");
+}
+
+function xiazaicishu(){
+document.writeln("下载次数：");
+}
+
+function xiazaisudu(){
+document.writeln("下载速度：");
+}
+
+function shoulushijian(){
+document.writeln("收录时间：");
+}
+
+function zuijinxiazai(){
+document.writeln("最近下载：");
+}
+
+function dmca(){
+document.writeln("DMCA/屏蔽：");
+}
+
+function cililianjie(){
+document.writeln("磁力链接");
+}
+
+function xunleixiazai(){
+document.writeln("迅雷下载");
+}
+
+function zhongzixiazai(){
+document.writeln("种子下载");
+}
+
+
