471940560f6c419e7798c00a97538efe8368dbd9
qqcdn <heiwu789@gmail.com>
2019-07-03 14:13:22 +0800

    Create uk.js

---
+++ js/uk.js
@@ -0,0 +1,247 @@
+//Home
+function benyueremenbtzhongzi(){
+Document.writeln("This month's popular BT seed");
+}
+
+function benzhouremenbtzhongzi(){
+Document.writeln("This week's hot BT seeds");
+}
+
+function jinriremenbtzhongzi(){
+Document.writeln("Today's Top BT Seeds");
+}
+
+function zuixinshoulucili(){
+Document.writeln("the latest magnetic record");
+}
+
+function yingpianpaihangbang(){
+Document.writeln("2019Video Leaderboard");
+}
+
+function zuixinmingxing(){
+Document.writeln("latest star");
+}
+
+function zuixinfanhao(){
+Document.writeln("newest number");
+}
+
+function btzhongzileixing(){
+Document.writeln("BT seed type");
+}
+
+function shipin(){
+Document.writeln("video");
+}
+
+function yinpin(){
+Document.writeln("audio");
+}
+
+function wendang(){
+Document.writeln("document");
+}
+
+function ruanjian(){
+Document.writeln("software/compressed package");
+}
+
+function tupian(){
+Document.writeln("picture");
+}
+
+function gaoqing(){
+Document.writeln("HD");
+}
+
+function youqinglianjie(){
+Document.writeln("Friendly Link");
+}
+
+function gengduo(){
+Document.writeln("More »");
+}
+
+
+/ / Search page
+function fenlei(){
+Document.writeln("Classification:");
+}
+
+function daxiao(){
+Document.writeln("Size:");
+}
+
+function redu(){
+Document.writeln("Hot:");
+}
+
+function shoulu(){
+Document.writeln("Include:");
+}
+
+function zuijin(){
+Document.writeln("Recent:");
+}
+
+function remensousuo(){
+Document.writeln("hot search");
+}
+
+function tebietishi(){
+Document.writeln("Special Tips");
+}
+
+function tebietishi1(){
+Document.writeln("This site does not store any resource content, only collects BT seed metadata and magnetic link. It is a completely legal search engine system. The website does not provide seed download service, nor is it responsible for the authenticity and legality of BT seeds. Please pay attention to the user!");
+}
+
+function paixu(){
+Document.writeln("sort:");
+}
+
+function quanbu(){
+Document.writeln("All");
+}
+
+function yingshi(){
+Document.writeln("film");
+}
+
+function yinyue(){
+Document.writeln("Music");
+}
+
+function tuxiang(){
+Document.writeln("image");
+}
+
+function anzhuangbao(){
+Document.writeln("installation package");
+}
+
+function yasuobao(){
+Document.writeln("compressed package");
+}
+
+function qita(){
+Document.writeln("Other");
+}
+
+function xiangsi(){
+Document.writeln("similar");
+}
+
+function redu1(){
+Document.writeln("heat");
+}
+
+function daxiao1(){
+Document.writeln("size");
+}
+
+function ruku(){
+Document.writeln("inbound");
+}
+
+function zuihou(){
+Document.writeln("Recent Download");
+}
+
+
+
+
+//content page
+
+function btzhongzimingcheng(){
+Document.writeln("BT seed name");
+}
+
+function btzhongzijibenxinxi(){
+Document.writeln("BT seed basic information");
+}
+
+function xiazaidizhi(){
+Document.writeln("download address");
+}
+
+function wendangliebiao(){
+Document.writeln("Document List");
+}
+
+function cainixihuan(){
+Document.writeln("Guess you like");
+}
+
+function zuixincililianjie(){
+Document.writeln("Latest Magnet Link");
+}
+
+function zhongzibiaoqian(){
+Document.writeln("seed tag");
+}
+
+function zhongzipingjia(){
+Document.writeln("seed evaluation");
+}
+
+function zuijinsousuo(){
+Document.writeln("Recent Search");
+}
+
+function renqimingxing(){
+Document.writeln("popular star");
+}
+
+function zuixinfanhao(){
+Document.writeln("newest number");
+}
+
+function zhongzihaxi(){
+Document.writeln("seed hash:");
+}
+
+function wendangdaxiao(){
+Document.writeln("Document size:");
+}
+
+function zhongzifenlei(){
+Document.writeln("seed classification:");
+}
+
+function wendangshuliang(){
+Document.writeln("number of documents:");
+}
+
+function xiazaicishu(){
+Document.writeln("Downloads:");
+}
+
+function xiazaisudu(){
+Document.writeln("Download speed:");
+}
+
+function shoulushijian(){
+Document.writeln("Include time:");
+}
+
+function zuijinxiazai(){
+Document.writeln("Recently downloaded:");
+}
+
+function dmca(){
+Document.writeln("DMCA/mask:");
+}
+
+function cililianjie(){
+Document.writeln("Magnetic Link");
+}
+
+function xunleixiazai(){
+Document.writeln("Thunder download");
+}
+
+function zhongzixiazai(){
+Document.writeln("seed download");
+}
