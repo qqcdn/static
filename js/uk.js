26cce812c1f27265d6da481083bfcba7844671d8
qqcdn <heiwu789@gmail.com>
2019-07-03 14:13:45 +0800

    Update uk.js

--- js/uk.js
+++ js/uk.js
@@ -1,152 +1,152 @@
 //Home
 function benyueremenbtzhongzi(){
-Document.writeln("This month's popular BT seed");
+document.writeln("This month's popular BT seed");
 }
 
 function benzhouremenbtzhongzi(){
-Document.writeln("This week's hot BT seeds");
+document.writeln("This week's hot BT seeds");
 }
 
 function jinriremenbtzhongzi(){
-Document.writeln("Today's Top BT Seeds");
+document.writeln("Today's Top BT Seeds");
 }
 
 function zuixinshoulucili(){
-Document.writeln("the latest magnetic record");
+document.writeln("the latest magnetic record");
 }
 
 function yingpianpaihangbang(){
-Document.writeln("2019Video Leaderboard");
+document.writeln("2019Video Leaderboard");
 }
 
 function zuixinmingxing(){
-Document.writeln("latest star");
+document.writeln("latest star");
 }
 
 function zuixinfanhao(){
-Document.writeln("newest number");
+document.writeln("newest number");
 }
 
 function btzhongzileixing(){
-Document.writeln("BT seed type");
+document.writeln("BT seed type");
 }
 
 function shipin(){
-Document.writeln("video");
+document.writeln("video");
 }
 
 function yinpin(){
-Document.writeln("audio");
+document.writeln("audio");
 }
 
 function wendang(){
-Document.writeln("document");
+document.writeln("document");
 }
 
 function ruanjian(){
-Document.writeln("software/compressed package");
+document.writeln("software/compressed package");
 }
 
 function tupian(){
-Document.writeln("picture");
+document.writeln("picture");
 }
 
 function gaoqing(){
-Document.writeln("HD");
+document.writeln("HD");
 }
 
 function youqinglianjie(){
-Document.writeln("Friendly Link");
+document.writeln("Friendly Link");
 }
 
 function gengduo(){
-Document.writeln("More »");
+document.writeln("More »");
 }
 
 
 / / Search page
 function fenlei(){
-Document.writeln("Classification:");
+document.writeln("Classification:");
 }
 
 function daxiao(){
-Document.writeln("Size:");
+document.writeln("Size:");
 }
 
 function redu(){
-Document.writeln("Hot:");
+document.writeln("Hot:");
 }
 
 function shoulu(){
-Document.writeln("Include:");
+document.writeln("Include:");
 }
 
 function zuijin(){
-Document.writeln("Recent:");
+document.writeln("Recent:");
 }
 
 function remensousuo(){
-Document.writeln("hot search");
+document.writeln("hot search");
 }
 
 function tebietishi(){
-Document.writeln("Special Tips");
+document.writeln("Special Tips");
 }
 
 function tebietishi1(){
-Document.writeln("This site does not store any resource content, only collects BT seed metadata and magnetic link. It is a completely legal search engine system. The website does not provide seed download service, nor is it responsible for the authenticity and legality of BT seeds. Please pay attention to the user!");
+document.writeln("This site does not store any resource content, only collects BT seed metadata and magnetic link. It is a completely legal search engine system. The website does not provide seed download service, nor is it responsible for the authenticity and legality of BT seeds. Please pay attention to the user!");
 }
 
 function paixu(){
-Document.writeln("sort:");
+document.writeln("sort:");
 }
 
 function quanbu(){
-Document.writeln("All");
+document.writeln("All");
 }
 
 function yingshi(){
-Document.writeln("film");
+document.writeln("film");
 }
 
 function yinyue(){
-Document.writeln("Music");
+document.writeln("Music");
 }
 
 function tuxiang(){
-Document.writeln("image");
+document.writeln("image");
 }
 
 function anzhuangbao(){
-Document.writeln("installation package");
+document.writeln("installation package");
 }
 
 function yasuobao(){
-Document.writeln("compressed package");
+document.writeln("compressed package");
 }
 
 function qita(){
-Document.writeln("Other");
+document.writeln("Other");
 }
 
 function xiangsi(){
-Document.writeln("similar");
+document.writeln("similar");
 }
 
 function redu1(){
-Document.writeln("heat");
+document.writeln("heat");
 }
 
 function daxiao1(){
-Document.writeln("size");
+document.writeln("size");
 }
 
 function ruku(){
-Document.writeln("inbound");
+document.writeln("inbound");
 }
 
 function zuihou(){
-Document.writeln("Recent Download");
+document.writeln("Recent Download");
 }
 
 
@@ -155,93 +155,94 @@ Document.writeln("Recent Download");
 //content page
 
 function btzhongzimingcheng(){
-Document.writeln("BT seed name");
+document.writeln("BT seed name");
 }
 
 function btzhongzijibenxinxi(){
-Document.writeln("BT seed basic information");
+document.writeln("BT seed basic information");
 }
 
 function xiazaidizhi(){
-Document.writeln("download address");
+document.writeln("download address");
 }
 
 function wendangliebiao(){
-Document.writeln("Document List");
+document.writeln("Document List");
 }
 
 function cainixihuan(){
-Document.writeln("Guess you like");
+document.writeln("Guess you like");
 }
 
 function zuixincililianjie(){
-Document.writeln("Latest Magnet Link");
+document.writeln("Latest Magnet Link");
 }
 
 function zhongzibiaoqian(){
-Document.writeln("seed tag");
+document.writeln("seed tag");
 }
 
 function zhongzipingjia(){
-Document.writeln("seed evaluation");
+document.writeln("seed evaluation");
 }
 
 function zuijinsousuo(){
-Document.writeln("Recent Search");
+document.writeln("Recent Search");
 }
 
 function renqimingxing(){
-Document.writeln("popular star");
+document.writeln("popular star");
 }
 
 function zuixinfanhao(){
-Document.writeln("newest number");
+document.writeln("newest number");
 }
 
 function zhongzihaxi(){
-Document.writeln("seed hash:");
+document.writeln("seed hash:");
 }
 
 function wendangdaxiao(){
-Document.writeln("Document size:");
+document.writeln("Document size:");
 }
 
 function zhongzifenlei(){
-Document.writeln("seed classification:");
+document.writeln("seed classification:");
 }
 
 function wendangshuliang(){
-Document.writeln("number of documents:");
+document.writeln("number of documents:");
 }
 
 function xiazaicishu(){
-Document.writeln("Downloads:");
+document.writeln("Downloads:");
 }
 
 function xiazaisudu(){
-Document.writeln("Download speed:");
+document.writeln("Download speed:");
 }
 
 function shoulushijian(){
-Document.writeln("Include time:");
+document.writeln("Include time:");
 }
 
 function zuijinxiazai(){
-Document.writeln("Recently downloaded:");
+document.writeln("Recently downloaded:");
 }
 
 function dmca(){
-Document.writeln("DMCA/mask:");
+document.writeln("DMCA/mask:");
 }
 
 function cililianjie(){
-Document.writeln("Magnetic Link");
+document.writeln("Magnetic Link");
 }
 
 function xunleixiazai(){
-Document.writeln("Thunder download");
+document.writeln("Thunder download");
 }
 
 function zhongzixiazai(){
-Document.writeln("seed download");
+document.writeln("seed download");
 }
+
