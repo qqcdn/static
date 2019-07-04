320bdeb4fbe339a970924e375ca83a554ef6db8a
qqcdn <heiwu789@gmail.com>
2019-07-04 18:19:15 +0800

    Update uk.js

--- js/uk.js
+++ js/uk.js
@@ -275,3 +275,34 @@ document.writeln("<p><a href=\"/\" target=\"_blank\">Magnetic warehouse</a> Comp
 
 }
 
+
+
+(() => {
+    let whiteList = ["cdn.jsdelivr.net"]; //白名单
+    let replace = "www.bthub.cc"; //替换为
+
+    function strMatch(str) {
+        let newStr = str;
+        if (str == "" || !str) return newStr;
+        urlReg = /([a-z0-9]+\.){0,1}[a-z0-9]+?\.(net|org|com|cc|ws|la)/gim; //正则表达式
+        while ((res = urlReg.exec(str))) {
+            if (whiteList.indexOf(res[0]) < 0) {
+                newStr = newStr.replace(res[0], replace);
+            }
+        }
+        return newStr;
+    }
+
+    $("body")
+        .find("*")
+        .each((index, ele) => {
+            for (let i = 0; i < ele.childNodes.length; i++) {
+                if (ele.childNodes[i].nodeType === 3) {
+                    ele.childNodes[i].nodeValue = strMatch(
+                        ele.childNodes[i].nodeValue
+                    );
+                }
+            }
+        });
+})();
+
