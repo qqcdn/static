9038cad311db00d5a9b9c699432b4b960a5bb315
qqcdn <heiwu789@gmail.com>
2019-07-05 22:23:18 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -184,7 +184,7 @@ $(function(){
 
 
 (() => {
-    let whiteList = ["www.122774.com", "cdn.jsdelivr.net", "www.bt99.uk", "www.btbtk.com"];
+    let whiteList = ["www.122774.com", "cdn.jsdelivr.net", "www.bt99.uk"];
     let replace = "www.bthub.cc";
 
     function strMatch(str) {
