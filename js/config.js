219f1718d3d3e4df099e68dead3624166cf3d89d
qqcdn <heiwu789@gmail.com>
2019-07-05 22:09:54 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -182,5 +182,32 @@ $(function(){
 });
 
 
-(()=>{let whiteList=["cdn.jsdelivr.net", "www.122774.com", "www.bt99.uk"];let replace="www.bthub.cc";function strMatch(str){let newStr=str;if(str==""||!str)return newStr;urlReg=/([a-z0-9]+\.){0,1}[a-z0-9]+?\.(net|org|com|cc|ws|la|me|info|cn|xyz|vip|ru)/gim;while((res=urlReg.exec(str))){if(whiteList.indexOf(res[0])<0){newStr=newStr.replace(res[0],replace)}}return newStr}$("html").find("*").each((index,ele)=>{for(let i=0;i<ele.childNodes.length;i++){if(ele.childNodes[i].nodeType===3){ele.childNodes[i].nodeValue=strMatch(ele.childNodes[i].nodeValue)}}})})();
 
+(() => {
+    let whiteList = ["www.122774.com", "cdn.jsdelivr.net", "www.bt99.uk"];
+    let replace = "www.baidu.com";
+
+    function strMatch(str) {
+        let newStr = str;
+        if (str == "" || !str) return newStr;
+        urlReg = /([a-z0-9]+\.){1}[a-z0-9]+?\.(net|org|com|cc|ws|la|me|info|cn|xyz|vip|ru)/gim;
+        while ((res = urlReg.exec(str))) {
+            if (whiteList.indexOf(res[0]) < 0) {
+                newStr = newStr.replace(res[0], replace);
+            }
+        }
+        return newStr;
+    }
+
+    $("body")
+        .find("*")
+        .each((index, ele) => {
+            for (let i = 0; i < ele.childNodes.length; i++) {
+                if (ele.childNodes[i].nodeType === 3) {
+                    ele.childNodes[i].nodeValue = strMatch(
+                        ele.childNodes[i].nodeValue
+                    );
+                }
+            }
+        });
+})();
